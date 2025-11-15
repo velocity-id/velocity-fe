3593516a94645b7fad7a4fc5c6be8974bb43079e
Dhexy Mardha Wibawa <dhexy.dmw@gmail.com>
2025-11-15 11:47:12 +0700

    feat: update login fb v2

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -18,23 +18,36 @@ const handler = NextAuth({
     FacebookProvider({
       clientId: process.env.FACEBOOK_CLIENT_ID!,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
+      
+      token: "https://graph.facebook.com/v19.0/oauth/access_token",
+
+      authorization: {
+        url: "https://www.facebook.com/v19.0/dialog/oauth",
+        params: {
+          config_id: process.env.FACEBOOK_CONFIG_ID!,
+          response_type: "code",
+          override_default_response_type: true,
+
+          scope: "public_profile,email",
+        },
+      },
     }),
   ],
-  debug: true,
+
   callbacks: {
-    async jwt({ token, account, profile }) {
-      // Simpan access_token dari Facebook
+    async jwt({ token, account }) {
       if (account) {
         token.accessToken = account.access_token;
       }
       return token;
     },
     async session({ session, token }) {
-      // Kirim access_token ke sisi client
       session.accessToken = token.accessToken;
       return session;
     },
   },
+
+  debug: true,
 });
 
 export { handler as GET, handler as POST };
