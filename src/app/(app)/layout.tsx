ad3f962e91326dfebb4887dd64e64c43ebf1e19d
gilangrp <gilangrpratama@gmail.com>
2025-12-13 13:36:37 +0700

    feat: get username in navbar using getServerSession

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,27 +1,30 @@
-import { cookies } from "next/headers"
+import { getServerSession } from "next-auth"
 
 import { AppSidebar } from "@/components/layout/app-sidebar"
 import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { LoadingProvider } from "@/hooks/use-loading"
 import { AlertProvider } from "@/hooks/use-alert"
+import { authOptions } from "../api/auth/[...nextauth]/route"
 
 export default async function Layout({ children }: React.PropsWithChildren) {
+  const session = await getServerSession(authOptions)
+
   return (
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset className="h-dvh">
-        <SidebarTrigger userName="Admin Bumi Tauhid" />
+        <SidebarTrigger
+          userName={session?.user?.name ?? "Admin"}
+        />
+
         <LoadingProvider>
           <AlertProvider>
-
-            <main className="px-10 py-5  gap-5 flex flex-col">
+            <main className="px-10 py-5 gap-5 flex flex-col">
               {children}
             </main>
           </AlertProvider>
         </LoadingProvider>
-
       </SidebarInset>
     </SidebarProvider>
-
   )
 }
